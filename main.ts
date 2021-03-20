4d2ecdaeb2020acbfa2173f53cd3e0af01b41fcc
Dave Chan <davechan@users.noreply.github.com>
2021-03-21 03:50:44 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 function get_distance () {
-    pins.setPull(DigitalPin.P2, PinPullMode.PullNone)
-    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.setPull(DigitalPin.P12, PinPullMode.PullNone)
+    pins.digitalWritePin(DigitalPin.P12, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P12, 1)
     control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    distance = pins.pulseIn(DigitalPin.P2, PulseValue.High)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    distance = pins.pulseIn(DigitalPin.P12, PulseValue.High)
     distance = Math.idiv(distance, 58)
 }
 let distance = 0

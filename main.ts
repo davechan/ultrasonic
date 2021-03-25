aaf05b13f685cb2b9b814f2ea00b265a81d1ae43
Dave Chan <davechan@users.noreply.github.com>
2021-03-26 00:45:50 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,20 @@
 function get_distance () {
-    pins.setPull(DigitalPin.P12, PinPullMode.PullNone)
-    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.setPull(DigitalPin.P8, PinPullMode.PullNone)
+    pins.digitalWritePin(DigitalPin.P8, 0)
     control.waitMicros(2)
-    pins.digitalWritePin(DigitalPin.P12, 1)
-    control.waitMicros(10)
-    pins.digitalWritePin(DigitalPin.P12, 0)
-    distance = pins.pulseIn(DigitalPin.P12, PulseValue.High)
-    distance = Math.idiv(distance, 58)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    control.waitMicros(50)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    distance = pins.pulseIn(DigitalPin.P8, PulseValue.High)
+    basic.showNumber(distance)
+    led.plotBarGraph(
+    distance,
+    0
+    )
 }
 let distance = 0
 distance = 0
 basic.forever(function () {
     get_distance()
-    basic.showNumber(distance)
     basic.pause(100)
 })
